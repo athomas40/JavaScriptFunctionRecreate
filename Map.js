206c27586038daea15ac65d7ff83be9ed0e735b5
Aleena Thomas <aleenathomas@Aleenas-MBP.lan>
2021-02-22 22:25:17 -0500

    comment fixed

--- Map.js
+++ Map.js
@@ -1,4 +1,4 @@
-//FOR EACH
+//MAP
 
 //"myMap" will take in an array of elements and creates a new array populated
 //with the results of calling a provided function on every element in the calling array.
